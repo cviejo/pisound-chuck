031cc355836f15871812c082b54c5612fb1c0fe5
cviejo <1179357+cviejo@users.noreply.github.com>
2024-10-12 00:01:25 +0100

    temp commit

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -e
 
 if [ "$CHUCK_BUILD" = "1" ]; then
-	git clone https://github.com/ccrma/chuck.git chuck-git
+	# git clone https://github.com/ccrma/chuck.git chuck-git
+	#
+	# apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
+	#
+	# cd chuck-git/src/
+	#
+	# make linux-jack
 
-	apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
-
-	cd chuck-git/src/
-
-	make linux-jack
-
-	cp chuck /usr/local/bin
+	cp ~/.bin/chuck /usr/local/bin
 else
+	apt-get update
 	apt-get install chuck
 fi
 
