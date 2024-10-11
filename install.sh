3078d8519a0e016a50b5bee4e188ef1f03770c03
cviejo <1179357+cviejo@users.noreply.github.com>
2024-10-12 00:03:05 +0100

    reverted changes

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
 if [ "$CHUCK_BUILD" = "1" ]; then
-	# git clone https://github.com/ccrma/chuck.git chuck-git
-	#
-	# apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
-	#
-	# cd chuck-git/src/
-	#
-	# make linux-jack
+	git clone https://github.com/ccrma/chuck.git chuck-git
 
-	cp ~/.bin/chuck /usr/local/bin
+	apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
+
+	cd chuck-git/src/
+
+	make linux-jack
+
+	cp chuck /usr/local/bin
 else
 	apt-get update
 	apt-get install chuck
