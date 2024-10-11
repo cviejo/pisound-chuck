bba74de2e3d8fb834cc3b6c4dd57b6e8bdcb3970
cviejo <1179357+cviejo@users.noreply.github.com>
2024-10-11 23:13:42 +0100

    removed sudo

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -e
 
-git clone https://github.com/ccrma/chuck.git chuck-git
+if [ "$CHUCK_BUILD" = "1" ]; then
+	git clone https://github.com/ccrma/chuck.git chuck-git
 
-sudo apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
+	apt-get install bison flex libasound2-dev libsndfile1-dev libjack-jackd2-dev
 
-cd chuck-git/src/
+	cd chuck-git/src/
 
-make linux-jack
-# make linux-alsa
+	make linux-jack
 
-mkdir ~/.bin
+	cp chuck /usr/local/bin
+else
+	apt-get install chuck
+fi
 
-cp chuck ~/.bin/chuck
-
-echo "chuck module installed in ~/.bin/chuck"
+echo "chuck module installed"
