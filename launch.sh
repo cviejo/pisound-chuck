d25eb749c930179848c2d4406de63d344c93a77e
cviejo <1179357+cviejo@users.noreply.github.com>
2024-10-12 16:15:42 +0100

    launch.sh update

--- launch.sh
+++ launch.sh
@@ -1,3 +1,18 @@
 #!/bin/sh -e
 
-chuck --loop
+. /usr/local/pisound/scripts/common/common.sh
+
+log "Killing all chuck instances!"
+killall chuck 2>/dev/null
+
+chuck -l &
+chuckPID=$!
+
+log "chuck started!"
+flash_leds 1
+sleep 0.3
+flash_leds 1
+sleep 0.3
+flash_leds 1
+
+wait_process $chuckPID
